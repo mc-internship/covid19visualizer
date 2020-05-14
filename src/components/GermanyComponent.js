2dfff5a2bf158e7b82a2d83b822d71a18ad0f02d
Aditi Sharma <aditi15ashley@gmail.com>
2020-05-14 18:48:35 +0530

    Frontend skeleton structure

---
+++ src/components/GermanyComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Germany(props) {
+    return(
+      <div className="container">
+        <h4>Germany</h4>
+      </div>
+    );
+}
+
+export default Germany;   
---
+++ src/components/MainComponent.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import Header from './HeaderComponent';
+import Home from './HomeComponent';
+import Impact from './ImpactComponent';
+import Timeline from './TimelineComponent';
+import Demographics from './DemographicsComponent';
+import Footer from './FooterComponent';
+import India from './IndiaComponent';
+import Usa from './UsaComponent';
+import Germany from './GermanyComponent';
+import Italy from './ItalyComponent';
+import Singapore from './SingComponent';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+
+
+class Main extends Component {
+
+    constructor(props){
+      super(props);
+
+      
+  }
+ 
+    render(){
+  
+
+        return (
+            <div>
+              <Header />
+              
+              <Switch>
+                <Route exact path = "/home" component = {Home} />
+                <Route exact path='/timeline' component={Timeline} />
+                <Route exact path = "/demographics" component = {Demographics} />
+                <Route exact path = "/impact" component = {Impact} />
+                <Route exact path = "/home/india" component = {India}/>
+                <Route exact path = "/home/usa" component = {Usa}/>
+                <Route exact path = "/home/germany" component = {Germany}/>
+                <Route exact path = "/home/italy" component = {Italy}/>
+                <Route exact path = "/home/singapore" component = {Singapore}/>
+                <Redirect to = "/home"/>
+              </Switch>
+              
+              <Footer />
+            </div>
+
+        );
+    }
+}
+
+export default Main;
