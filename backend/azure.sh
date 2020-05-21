60d5666bc5419b4af6a8f6b74a8338fbec6b2fb1
Yugesh Kothari <yugesh.kothari@gmail.com>
2020-05-21 19:55:47 +0530

    Updated package.json engine and Azure drivers

---
+++ backend/azure.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list
+apt-get update
+ACCEPT_EULA=Y apt-get install msodbcsql17
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+source ~/.bashrc
+apt-get install unixodbc-dev
+apt-get install libgssapi-krb5-2
